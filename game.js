2b56d2a3c475d1647dad91050b4639bb429133c0
Gian D'Ambrosio <gpreed55@gmail.com>
2019-09-16 23:56:06 +0200

    Game done!

--- game.js
+++ game.js
@@ -12,30 +12,59 @@ function computerPlay(){
     }
 }
 
-const playerSelection = prompt('Input your choice:').toLowerCase();
 const computerSelection = computerPlay();
 
 function gamePlay(playerSelection, computerSelection){
     
     if(playerSelection ===  'rock'){
-        if(computerSelection === 'Rock'){return 'draw';}
-        else if(computerSelection === 'Scissors'){return 'win';}
-        else if(computerSelection === 'Paper'){return 'lose';}
+        if(computerSelection === 'Rock'){ alert('DRAW!'); return 'draw';}
+        else if(computerSelection === 'Scissors'){ alert('You WIN!'); return 'win1';}
+        else if(computerSelection === 'Paper'){ alert('You Lose!'); return 'lose2';}
     }else if(playerSelection ===  'scissors'){
-        if(computerSelection === 'Rock'){return 'lose';}
-        else if(computerSelection === 'Scissors'){return 'draw';}
-        else if(computerSelection === 'Paper'){return 'win';}
+        if(computerSelection === 'Rock'){ alert('You Lose!'); return 'lose2';}
+        else if(computerSelection === 'Scissors'){ alert('DRAW!'); return 'draw';}
+        else if(computerSelection === 'Paper'){ alert('You WIN!'); return 'win1';}
     }else if(playerSelection ===  'paper'){
-        if(computerSelection === 'Rock'){return 'win';}
-        else if(computerSelection === 'Scissors'){return 'lose';}
-        else if(computerSelection === 'Paper'){return 'draw';}    
+        if(computerSelection === 'Rock'){ alert('You WIN!'); return 'win1';}
+        else if(computerSelection === 'Scissors'){ alert('You Lose!'); return 'lose2';}
+        else if(computerSelection === 'Paper'){ alert('DRAW!'); return 'draw';}    
     }
 }
+   
 
+function game(times){
 
-function game(){
-
+    let count1 = 0;
+    let count2 = 0;
+    let ties = 0;
+    i = 0;
     
+
+    while(i < times){
     
+    //PlayerSelection Input...
+    const playerSelection = prompt('Input your choice:').toLowerCase();
+    
+    let resultGame = gamePlay(playerSelection, computerSelection);
+    
+    //Win counter
+    if(resultGame === 'win1'){
+        count1++;
+    }else if(resultGame === 'lose2'){
+        count2++;
+    }else if(resultGame === 'draw'){
+        ties++;
+    }
+    i++;
+    } //end of Loop
+
+    //Final Score
+    if(count1 > count2){
+        console.log('And the winner is: Player 1 with '+ count1 + ' - ' + count2 + '. Ties : ' + ties );
+    }else if (count2 > count1){
+        console.log('And the winner is The Computer! with '+ count2+ ' - ' + count1 + '. Ties : ' + ties);
+    }else{
+        console.log('Ended on draw, number of ties: '+ ties);
+    }
 }
-game();
+game(5);
