fe947b8577ecb69a65d4cc8137f3b781111957d6
Gian D'Ambrosio <gpreed55@gmail.com>
2019-09-16 22:25:24 +0200

    Only game function left

--- game.js
+++ game.js
@@ -1,1 +1,41 @@
-console.log('Hi');
+function computerPlay(){
+    var random = Math.floor((Math.random() * 9) + 1);
+
+    if(random <= 3 && random >= 0){
+        return 'Rock';
+    }
+    else if( random > 3 && random <= 6){
+        return 'Paper';
+    }
+    else if (random > 6 && random <= 9){
+        return 'Scissors';
+    }
+}
+
+const playerSelection = prompt('Input your choice:').toLowerCase();
+const computerSelection = computerPlay();
+
+function gamePlay(playerSelection, computerSelection){
+    
+    if(playerSelection ===  'rock'){
+        if(computerSelection === 'Rock'){return 'draw';}
+        else if(computerSelection === 'Scissors'){return 'win';}
+        else if(computerSelection === 'Paper'){return 'lose';}
+    }else if(playerSelection ===  'scissors'){
+        if(computerSelection === 'Rock'){return 'lose';}
+        else if(computerSelection === 'Scissors'){return 'draw';}
+        else if(computerSelection === 'Paper'){return 'win';}
+    }else if(playerSelection ===  'paper'){
+        if(computerSelection === 'Rock'){return 'win';}
+        else if(computerSelection === 'Scissors'){return 'lose';}
+        else if(computerSelection === 'Paper'){return 'draw';}    
+    }
+}
+
+
+function game(){
+
+    
+    
+}
+game();
